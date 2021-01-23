0dfb1ac2f8a3857acb28c87d466a9229d10750b1
Trybadisch <jmontoliutri@gmail.com>
2021-01-23 18:34:05 +0100

    Add Flask

--- application.py
+++ application.py
@@ -4,4 +4,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template("index.html")  # from templates/
+	return render_template("index.html")  # from templates
+
+@app.route("/data")
+def data():
+	return render_template("data.html")
+
+@app.route("/terminal")
+def terminal():
+	return render_template("terminal.html")
+
+@app.route("/dashboard")
+def dashboard():
+	return render_template("dashboard.html")
+
+
+@app.route("/about")
+def about():
+	return render_template("about.html")
